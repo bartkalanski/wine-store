5c073d5c94e5b7a5abe8fd20824f006e5a83b6ed
Bart Kalanski <bkalanski@gmail.com>
2021-01-03 17:57:02 +0000

    Added StoreList component

--- src/components/routes/Store.js
+++ src/components/routes/Store.js
@@ -1,40 +1,16 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import { addToBasket } from "../../actions/cartActions";
 import "./Store.css";
+import StoreList from "./StoreList";
 
 class Store extends Component {
-  handleClick(id) {
-    this.props.addToBasketDispatch(id);
-  }
   render() {
-    const itemList = this.props.items.map((item) => {
-      return (
-        <div class="six wide column">
-          <div class="ui card">
-            <img class="image" src={item.image} alt={item.title} />
-            <div class="content">
-              <div class="center aligned header">{item.title}</div>
-              <div className="description">
-                <div class="description">{item.type}</div>
-                <div class="description">{item.origin}</div>
-              </div>
-            </div>
-            <div class="left aligned extra content">{item.price}</div>
-            <button
-              onClick={() => this.handleClick(item.id)}
-              class="orange ui button"
-            >
-              Add To Basket
-            </button>
-          </div>
-        </div>
-      );
-    });
     return (
       <div className="ui container">
-        <div className="ui grid">{itemList}</div>
+        <div className="ui grid">
+          <StoreList items={this.props.items} />
+        </div>
       </div>
     );
   }
@@ -46,12 +22,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addToBasketDispatch: (id) => {
-      dispatch(addToBasket(id));
-    },
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Store);
+export default connect(mapStateToProps)(Store);
---
+++ src/components/routes/StoreList.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+
+import { addToBasket } from "../../actions/cartActions";
+
+const StoreList = ({ items }) => {
+    const dispatch = useDispatch()
+ 
+    return items.map((item) => {
+        return (
+          <div class="six wide column">
+            <div class="ui card">
+              <img class="image" src={item.image} alt={item.title} />
+              <div class="content">
+                <div class="center aligned header">{item.title}</div>
+                <div className="description">
+                  <div class="description">{item.type}</div>
+                  <div class="description">{item.origin}</div>
+                </div>
+              </div>
+              <div class="left aligned extra content">{item.price}</div>
+              <button
+                onClick={() => dispatch(addToBasket(item.id))}
+                class="orange ui button"
+              >
+                Add To Basket
+              </button>
+            </div>
+          </div>
+        );
+      });
+}
+
+export default StoreList
