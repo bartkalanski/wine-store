83b6ab1a5c59c21eacadfdb7db7cefdf80ea9f30
Bart Kalanski <bkalanski@gmail.com>
2021-01-04 14:47:03 +0000

    Fixed total price bug

--- src/components/routes/Basket.js
+++ src/components/routes/Basket.js
@@ -5,6 +5,13 @@ import "./Basket.css";
 import BasketList from "./BasketList";
 
 class Basket extends React.Component {
+  formatPrice(totalPrice) {
+    const formatTotalPrice = {
+      style: "currency",
+      currency: "GBP",
+    };
+    return totalPrice.toLocaleString("en-GB", formatTotalPrice);
+  }
   render() {
     if (this.props.items.length) {
       return (
@@ -14,7 +21,7 @@ class Basket extends React.Component {
           </div>
           <div className="total__container">
             <div className="total__price">
-              Total to pay: £{this.props.total}
+              Total to pay: {this.formatPrice(this.props.total)}
             </div>
 
             <button className="ui orange button total__btn">
