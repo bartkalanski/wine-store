51ab2028dd0aa263178bc6e9daec6448bce1fd6e
Bart Kalanski <bkalanski@gmail.com>
2021-01-05 22:09:43 +0000

    uploading for overview

--- src/reducers/basketReducer.js
+++ src/reducers/basketReducer.js
@@ -1,41 +1,78 @@
-import products from '../products/products.json'
-import { ADD_TO_BASKET } from '../actions/action-types/cart-actions'
+import products from "../products/products.json";
+import { ADD_TO_BASKET } from "../actions/action-types/cart-actions";
 
 const initState = {
-    items: products.items,
-    itemsInBasket: [],
-    total: 0,
-    quantity: 0
-}
+  items: products.items,
+  itemsInBasket: [],
+  total: 0,
+  basketQuantity: 0,
+};
 
-const basketReducer = (state = initState, action ) => {
-    if(action.type === ADD_TO_BASKET){
-        // Check if selected wine matches Wines in state
-       const addItem = state.items.find(item => item.id === action.id)
-       // check if action id exists in itemsInBasket
-       const basketIsNotEmpty = state.itemsInBasket.find(item => action.id === item.id)
-       
-       if(basketIsNotEmpty){
-           addItem.quantity++
-           return {
-               ...state,
-                total: parseFloat(state.total) + parseFloat(addItem.price),
-                quantity: state.quantity + 1
-           }
-       }
-       else{
-    
-           addItem.quantity = 1
-           return {
-               ...state,
-               itemsInBasket: [...state.itemsInBasket, addItem],
-               total: parseFloat(state.total) + parseFloat(addItem.price),
-               quantity: state.quantity + 1
-           }
-       }
-       
+const basketReducer = (state = initState, action) => {
+  if (action.type === ADD_TO_BASKET) {
+    // Check if selected wine matches wines in state
+    const addItem = { ...state.items.find((item) => action.id === item.id) };
+    // Check if selected wine is in the basket
+    const wineIsInBasket = state.itemsInBasket.find(
+      (item) => action.id === item.id
+    );
+    if (wineIsInBasket) {
+      addItem.quantity++;
+      return {
+        ...state,
+        total: parseFloat(state.total) + parseFloat(addItem.price),
+        basketQuantity: state.basketQuantity + 1,
+      };
+    } else {
+      addItem.quantity = 1;
+      return {
+        ...state,
+        itemsInBasket: [...state.itemsInBasket, addItem],
+        total: parseFloat(state.total) + parseFloat(addItem.price),
+        basketQuantity: state.basketQuantity + 1,
+      };
     }
-    return state
-}
+  }
+  return state;
+};
 
 export default basketReducer;
+
+/*if (basketIsNotEmpty) {
+      console.log("copyAddItem", addItem);
+      addItem.quantity += 1;
+
+      return {
+        ...state,
+        total: parseFloat(state.total) + parseFloat(addItem.price),
+        quantity: state.quantity + 1,
+      };
+    } else {
+      console.log(addItem.quantity);
+      return {
+        ...state,
+        itemsInBasket: [...state.itemsInBasket, addItem],
+        total: parseFloat(state.total) + parseFloat(addItem.price),
+        quantity: state.quantity + 1,
+      };
+    } */
+
+/*
+    
+    
+    if (addItem.quantity === undefined) {
+      addItem.quantity = 1;
+      return {
+        ...state,
+        itemsInBasket: [...state.itemsInBasket, addItem],
+        total: parseFloat(state.total) + parseFloat(addItem.price),
+        quantity: state.quantity + 1,
+      };
+    } else {
+      addItem.quantity++;
+      return {
+        ...state,
+        total: parseFloat(state.total) + parseFloat(addItem.price),
+        quantity: state.quantity + 1,
+      };
+    }*/
