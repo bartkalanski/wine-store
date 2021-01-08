f0c26528af2ce1a354b1aaf809a18836b7f543f0
Bart Kalanski <bkalanski@gmail.com>
2021-01-08 23:40:48 +0000

    Added more tests

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,24 +1,34 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { shallow } from "enzyme";
+
+import * as reactRedux from "react-redux";
 
 import Header from "../components/Header";
-import basketReducer from "../reducers/basketReducer";
-
-const store = createStore(basketReducer);
+import { Link } from "react-router-dom";
 
 describe("Header", () => {
+  const useSelectorMock = jest.spyOn(reactRedux, "useSelector");
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(
-      <Provider store={store}>
-        <Header />
-      </Provider>
-    );
+    useSelectorMock.mockClear();
+    wrapper = shallow(<Header />);
   });
   it("should render without throwing an error", () => {
     expect(wrapper).not.toBeNull();
+  });
+  it("should render Basket Link tag", () => {
+    expect(
+      wrapper.containsMatchingElement(<Link to="/basket">Basket</Link>)
+    ).toEqual(true);
+  });
+  it("should not render any numbers over basket if no items in the basket are available", () => {
+    const basket = wrapper.find(".shopping__basket-items");
+    expect(basket.text()).toBe("");
+  });
+  it("should render number of items in the basket", () => {
+    useSelectorMock.mockReturnValue({ basketQuantity: 2 })
+    const basket = wrapper.find(".shopping__basket-items");
+    expect(basket.text()).toBe(1);
   });
 });
