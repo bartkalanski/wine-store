341d0a1975ded5181a93fbc8e9fbbb81e51e7605
Bart Kalanski <bkalanski@gmail.com>
2021-01-15 03:20:40 +0000

    updated basket and basket list design

--- src/components/routes/BasketList.js
+++ src/components/routes/BasketList.js
@@ -10,6 +10,7 @@ const BasketList = ({ items }) => {
     return (
       <React.Fragment>
         <div className="basket__holder">
+          <div className="basket__divider">
           <div className="basket__img">
             <img
               className="basket__img-img"
@@ -17,19 +18,21 @@ const BasketList = ({ items }) => {
               alt={item.title}
             />
           </div>
-          <div className="basket__data">
-            <div className="basket__name">Name: {item.title}</div>
-            <div className="basket__origin">
-              Country of Origin: {item.origin}
-            </div>
-            <div className="basket__price">Price: £ {item.price}</div>
-            <div className="basket__quantity">Quantity: {item.quantity}</div>
+          <div className="basket__remove">
+            <div className="basket__name">{item.title}</div>
             <button
               className="ui negative basic button"
               onClick={() => dispatch(removeFromBasket(item.id))}
             >
               Remove Item
             </button>
+          </div></div>
+          <div className="basket__data">
+            <div className="basket__origin">
+              {item.origin}
+            </div>
+            <div className="basket__price">{item.price}</div>
+            <div className="basket__quantity">{item.quantity}</div>
           </div>
         </div>
       </React.Fragment>
