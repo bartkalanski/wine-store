7b640b53b66e66a596b2a1381558beebb92f685a
Bart Kalanski <bkalanski@gmail.com>
2021-01-17 19:11:36 +0000

    Redesigned Basket component

--- src/components/routes/BasketList.js
+++ src/components/routes/BasketList.js
@@ -11,26 +11,26 @@ const BasketList = ({ items }) => {
       <React.Fragment>
         <div className="basket__holder">
           <div className="basket__divider">
-          <div className="basket__img">
-            <img
-              className="basket__img-img"
-              src={getImageFromJson(item.image)}
-              alt={item.title}
-            />
-          </div>
-          <div className="basket__remove">
-            <div className="basket__name">{item.title}</div>
-            <button
-              className="ui negative basic button"
-              onClick={() => dispatch(removeFromBasket(item.id))}
-            >
-              Remove Item
-            </button>
-          </div></div>
-          <div className="basket__data">
-            <div className="basket__origin">
-              {item.origin}
+            <div className="basket__img">
+              <img
+                className="basket__img-img"
+                src={getImageFromJson(item.image)}
+                alt={item.title}
+              />
             </div>
+            <div className="basket__remove">
+              <div className="basket__name">{item.title}</div>
+              <button
+                className="ui negative basic button"
+                onClick={() => dispatch(removeFromBasket(item.id))}
+              >
+                Remove Item
+              </button>
+            </div>
+          </div>
+
+          <div className="basket__data">
+            <div className="basket__origin">{item.origin}</div>
             <div className="basket__price">{item.price}</div>
             <div className="basket__quantity">{item.quantity}</div>
           </div>
